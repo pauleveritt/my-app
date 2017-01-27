0bab99dbefff24922d506e5b056f61f644ba3297
Paul Everitt <pauleveritt@me.com>
2017-01-27 09:43:08 -0500

    First cut at Wallaby.

---
+++ wallaby.js
@@ -0,0 +1,43 @@
+const wallabyWebpack = require('wallaby-webpack');
+const webpackPostprocessor = wallabyWebpack({});
+const path = require('path');
+
+module.exports = function (wallaby) {
+    process.env.NODE_PATH +=
+        path.delimiter +
+        path.join(__dirname, 'node_modules') +
+        path.delimiter +
+        path.join(__dirname, 'node_modules/react-scripts-ts/node_modules');
+    require('module').Module._initPaths();
+
+    // Babel needs this
+    // create-react-app-typescript does not use this
+    // process.env.NODE_ENV = 'development';
+
+    return {
+        files: [
+            'src/**/*.ts*',
+            '!src/**/*.test.ts*'
+        ],
+
+        tests: ['src/**/*.test.ts*'],
+
+        env: {
+            type: 'node',
+            runner: 'node'
+        },
+
+        postprocessor: webpackPostprocessor,
+
+        setup: (wallaby) => {
+            wallaby.testFramework.configure({
+                moduleNameMapper: {
+                    '^.+\\.(jpg|jpeg|png|gif|svg)$': require.resolve('react-scripts-ts/config/jest/fileTransform.js'),
+                    '^.+\\.css$': require.resolve('react-scripts-ts/config/jest/cssTransform.js')
+                }
+            });
+        },
+
+        testFramework: 'jest'
+    };
+};
